3cfc00f4cddf4d31f724b78be935c4810a6436b6
nguyenvu0911 <160732808+nguyenvu0911@users.noreply.github.com>
2024-02-25 02:52:15 +0700

    Add files via upload
    create card 2

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Send for you !';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100005644339154';
+const eventName = 'This is small gift';
+const titleCard = 'BLINH BLINH';
+const contentCard = 'Chúc bạn 1 ngày mới tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với bạn trong hôm nay và cả những ngày sau,hi.💘';
+
+const giftImage = 'anhban.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
